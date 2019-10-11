8937752a1a0e097e610450b37ddb7437419f2999
Matheus <mrodriguesle@gmail.com>
2019-10-11 01:37:21 -0300

    Second Commit

---
+++ app/src/main/java/com/tc/nutriyou/activity/HomePage.java
@@ -0,0 +1,49 @@
+package com.tc.nutriyou.activity;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.tc.nutriyou.R;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+public class HomePage extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homepage);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+
+                    switch (item.getItemId()){
+                        case R.id.nav_eu:
+                            selectedFragment = new EuPage();
+                            break;
+                        case R.id.nav_planos:
+                            selectedFragment = new PlanosPage();
+                            break;
+                        case R.id.nav_cardapios:
+                            selectedFragment = new CardapiosPage();
+                            break;
+                        case R.id.nav_pedidos:
+                            selectedFragment = new PedidosPage();
+                            break;
+                    }
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+                    return true;
+                }
+            };
+
+}
---
+++ app/src/main/java/com/tc/nutriyou/activity/PedidosPage.java
@@ -0,0 +1,21 @@
+package com.tc.nutriyou.activity;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.tc.nutriyou.R;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class PedidosPage extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.activity_pedidospage, container, false);
+    }
+}
